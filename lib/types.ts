f3ec98b0fcd9219177f705c68733aa814262535d
Ly <developer.lenneth@gmail.com>
2022-01-28 23:57:10 -0800

    Fixed script type

--- lib/types.ts
+++ lib/types.ts
@@ -1,18 +1,19 @@
-export type Coin = "btc" | "dash" | "doge" | "ltc" | "bcy";
-export type Chain = "main" | "test3" | "test";
+export type Coin = 'btc' | 'dash' | 'doge' | 'ltc' | 'bcy';
+export type Chain = 'main' | 'test3' | 'test';
 export type EventType =
-  | "unconfirmed-tx"
-  | "new-block"
-  | "confirmed-tx"
-  | "tx-confirmation"
-  | "double-spend-tx"
-  | "tx-confidence";
+  | 'unconfirmed-tx'
+  | 'new-block'
+  | 'confirmed-tx'
+  | 'tx-confirmation'
+  | 'double-spend-tx'
+  | 'tx-confidence';
+
 export type ScriptType =
-  | "pay-to-public-key-hash"
-  | "pay-to-public-key"
-  | "null-data"
-  | "multisig"
-  | "pay-to-script-hash"
-  | "pay-to-witness-pubkeyhash"
-  | "pay-to-witness-scripthash"
-  | "witness-unknown";
+  | 'pay-to-pubkey-hash'
+  | 'pay-to-multi-pubkey-hash'
+  | 'pay-to-pubkey'
+  | 'pay-to-script-hash'
+  | `multisig-${number}-of-${number}`
+  | 'null-data'
+  | 'empty'
+  | 'unknown';
