7b8e65df5b5a7e51b43e6e8a2a0b17d6b94ba95d
Lenneth <developer.lenneth@gmail.com>
2022-01-29 19:38:24 -0800

    Update bitcoinjs-blockcypher.ts
    Made the prefixes more aligned to the bitcoin testnet and improved functionality

--- recipes/bitcoinjs-blockcypher.ts
+++ recipes/bitcoinjs-blockcypher.ts
@@ -1,58 +1,85 @@
-import { script } from 'bitcoinjs-lib';
-import type { Network } from 'bitcoinjs-lib';
-import * as ecc from 'tiny-secp256k1';
-import { ECPairFactory } from 'ecpair';
-import type { TransactionSkeleton } from '../lib';
+import type { TransactionSkeleton } from "blockcypher-apis";
+import { script } from "bitcoinjs-lib";
+import type { Network } from "bitcoinjs-lib";
+import { ECPairFactory } from "ecpair";
+import type { ECPairInterface } from "ecpair";
+import * as ecc from "tiny-secp256k1";
 
 const ECPair = ECPairFactory(ecc);
 
 export const BlockCypherNetwork: Network = {
-  messagePrefix: '\x18Bitcoin Signed Message:\n',
-  bech32: 'bcy',
+  messagePrefix: "\x18Bitcoin Signed Message:\n",
+  bech32: "bcy",
+  // bip32 & bip44
   bip32: {
-    public: 0x0488b21e,
-    private: 0x0488ade4,
+    // B/c their bip84 prefixes align w/ the testnet prefixes
+    // I assume that the same applies to bip32
+    public: 0x043587cf,
+    private: 0x04358394,
+    //public: 0x0488b21e,
+    //private: 0x0488ade4,
   },
   pubKeyHash: 0x1b,
   scriptHash: 0x1f,
   wif: 0x49,
 };
 
+export const BlockCypherNetworkBip84 = {
+  public: 0x045f1cf6,
+  private: 0x045f18bc,
+};
+
 export const fromWIF = (wif: string, network?: Network) =>
   ECPair.fromWIF(wif, network);
 
 export const fromPublicKeyHex = (publicKeyHex: string, network?: Network) => {
-  const publicKey = Buffer.from(publicKeyHex, 'hex');
+  const publicKey = Buffer.from(publicKeyHex, "hex");
   return ECPair.fromPublicKey(publicKey, { network });
 };
 
 export const fromPrivateKeyHex = (privateKeyHex: string, network?: Network) => {
-  const privateKey = Buffer.from(privateKeyHex, 'hex');
+  const privateKey = Buffer.from(privateKeyHex, "hex");
   return ECPair.fromPrivateKey(privateKey, { network });
 };
 
-export const signTransactionSkeleton = (
-  keyPairHex: {
-    publicKey: string;
-    privateKey: string;
-    network?: Network;
-  },
-  transactionSkeleton: TransactionSkeleton,
-) => {
-  const keyPair = fromPrivateKeyHex(keyPairHex.privateKey, keyPairHex.network);
+export const getSignatureHex = (
+  keyPair: ECPairInterface,
+  dataHex: string,
+  hashType: number = 0x01
+) =>
+  script.signature
+    .encode(keyPair.sign(Buffer.from(dataHex, "hex")), hashType)
+    .toString("hex");
+
+export interface SignTransactionOptions {
+  keyPair: ECPairInterface;
+  transactionSkeleton: TransactionSkeleton;
+  network?: Network;
+  /** BlockCypher automatically appends the SIGHASH_ALL OP code at the end of non-segwit transactions */
+  isSegWit?: boolean;
+  //* https://en.bitcoin.it/wiki/OP_CHECKSIG */
+  hashType?: number;
+}
+
+export const signTransactionSkeleton = ({
+  keyPair,
+  transactionSkeleton,
+  isSegWit,
+  hashType,
+}: SignTransactionOptions) => {
+  if (!isSegWit && hashType)
+    throw new Error(
+      "Non-SegWit BlockCypher transactions can not have a hash type"
+    );
+  if (isSegWit && !hashType)
+    throw new Error("SegWit transactions must have a hash type");
   transactionSkeleton.pubkeys = [];
   transactionSkeleton.signatures = [];
-
-  // The hash type doesn't actually matter in this since it must be removed
-  // or BlockCypher will complain
-  const getSignatureHex = (dataHex: string) =>
-    script.signature
-      .encode(keyPair.sign(Buffer.from(dataHex, 'hex')), 0x01)
-      .toString('hex')
-      .slice(0, -2);
-
+  const publicKeyHex = keyPair.publicKey.toString("hex");
   for (const dataHex of transactionSkeleton.tosign) {
-    transactionSkeleton.pubkeys.push(keyPairHex.publicKey);
-    transactionSkeleton.signatures.push(getSignatureHex(dataHex));
+    transactionSkeleton.pubkeys.push(publicKeyHex);
+    let signatureHex = getSignatureHex(keyPair, dataHex, hashType);
+    if (!isSegWit) signatureHex = signatureHex.slice(0, -2);
+    transactionSkeleton.signatures.push(signatureHex);
   }
 };
